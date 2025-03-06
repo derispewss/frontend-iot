f4ff714fcb6d11027fcceaa530875609e2991273
derispewss <derisfirmansyah177@gmail.com>
2025-03-06 19:55:01 +0700

    feat: add utility functions and UI components for improved layout and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+
 export default function Home() {
-  return <p>hello world</p>
+  return (
+    <div className="container mx-auto bg-amber-400 min-h-screen flex items-center justify-center">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle>Welcome to Application Management IOT</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="mb-4">Created By Group A</p>
+          <Button>Click me</Button>
+        </CardContent>
+      </Card>
+    </div>
+  )
 }
