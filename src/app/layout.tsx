c158073b4f4c3d735bf439d89adb8c4bef1b8ed2
zappto <saptogusty@gmail.com>
2025-03-26 05:48:09 +0700

    Feat: implement page indicator and navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,31 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+const poppins = Poppins({
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
-  title: "IOT Monitoring System",
-  description: "Created by Group A",
+    title: "IOT Monitoring System",
+    description: "Created by Group A",
 };
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode;}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang="en">
+            <body className={`${geistSans.variable} ${geistMono.variable} ${poppins.className} antialiased`}>{children}</body>
+        </html>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,119 @@
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+"use client";
+
+import Logo from "@/components/image/Logo.png";
+import Image from "next/image";
+import { KoHo } from "next/font/google";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+const koho = KoHo({
+    subsets: ["latin"],
+    weight: ["400"],
+});
 
 export default function Home() {
-  return (
-    <div className="container mx-auto bg-amber-400 min-h-screen flex items-center justify-center">
-      <Card className="w-full max-w-md">
-        <CardHeader>
-          <CardTitle>Welcome to Application Management IOT</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p className="mb-4">Created By Group A</p>
-          <Button>Click me</Button>
-        </CardContent>
-      </Card>
-    </div>
-  )
+    const router = useRouter();
+    const [showEnterance, setShowEnterance] = useState(true);
+    const [isFade, setIsFade] = useState(false);
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const pages = [
+        {
+            title: "Open trash can with sensor",
+            description: "we can open the trash can if someone approaches the sensor",
+        },
+        {
+            title: "Monitoring trash bin capacity",
+            description: "capacity monitoring that we can do through this website",
+        },
+        {
+            title: "Determining the schedule for garbage disposal",
+            description: "creating a waste disposal schedule when the waste has reached the maximum limit ",
+        },
+        {
+            title: "Please install telegram ",
+            description: "Please install telegram for monitoring notification from your garbage",
+        },
+    ];
+
+    const handleNext = () => {
+        if (currentPage === pages.length - 1) {
+            router.push("/"); //belum ditentuin mau kemana (belum ada routesnya)
+        }
+        setIsFade(false);
+        setTimeout(() => {
+            setCurrentPage((prev) => (prev + 1) % pages.length);
+            setIsFade(true);
+        }, 300);
+    };
+
+    useEffect(() => {
+        const setTime = setTimeout(() => {
+            setShowEnterance(false);
+        }, 3000);
+
+        const delay = setTimeout(() => {
+            setIsFade(true);
+        }, 2500);
+
+        return () => {
+            clearTimeout(setTime);
+            clearTimeout(delay);
+        };
+    }, []);
+
+    return (
+        <>
+            {showEnterance ? (
+                <div
+                    className={`absolute inset-0 z-10 flex items-center bg-background justify-center transition-opacity duration-1000 ease-in-out ${
+                        isFade ? "opacity-0" : "opacity-100"
+                    }`}
+                >
+                    <div className="flex flex-row justify-center items-center">
+                        <Image src={Logo} alt="Logo" />
+                        <h1 className={`${koho.className} text-6xl pl-2.5`}>EcoTrack</h1>
+                    </div>
+                </div>
+            ) : (
+                !showEnterance && (
+                    <div className="min-h-screen flex flex-col items-center justify-center px-4 py-8 bg-background">
+                        <div className="flex-grow flex flex-col items-center justify-center">
+                            <div className={`container mx-auto transition-opacity duration-1000 ease-in-out ${isFade ? "opacity-100" : "opacity-0"}`}>
+                                <div className="flex flex-row justify-center items-center">
+                                    <Image src={Logo} alt="Logo" />
+                                    <h1 className={`${koho.className} text-6xl pl-2.5`}>EcoTrack</h1>
+                                </div>
+                                <div className="mt-6 text-center">
+                                    <p className="font-bold text-xl pb-2.5">{pages[currentPage].title}</p>
+                                    <p className="text-sidebar-ring font-light text-2xl">{pages[currentPage].description}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="mt-auto mb-6 w-full relative flex justify-center items-center">
+                            <div className="flex space-x-2">
+                                {pages.map((_, index) => (
+                                    <div
+                                        key={index}
+                                        onClick={() => {
+                                            setIsFade(false);
+                                            setTimeout(() => {
+                                                setCurrentPage(index);
+                                                setIsFade(true);
+                                            }, 100);
+                                        }}
+                                        className={`w-20 h-4 rounded hover:cursor-pointer ${index === currentPage ? "bg-black" : "bg-zinc-200"}`}
+                                    />
+                                ))}
+                            </div>
+                            <Button className="absolute right-4 mr-4 bg-zinc-200 text-black hover:bg-white hover:cursor-pointer w-24 h-10" onClick={handleNext}>
+                                {currentPage === pages.length - 1 ? "Start" : "Next"}
+                            </Button>
+                        </div>
+                    </div>
+                )
+            )}
+        </>
+    );
 }
